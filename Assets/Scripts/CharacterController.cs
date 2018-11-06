f5fb10a8d24f9c042a25bd51d4ba9e95b03de0bd
Vladimir <drumscum13@gmail.com>
2018-11-06 03:30:15 +0300

    Simple Scene Placement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    GameObject gameObject;
+    float speed = 3.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        var weapon = GameObject.FindGameObjectWithTag("Weapon");
+
+        if (Input.GetKeyDown("z"))
+        {
+            speed = 0f;
+            weapon.transform.Rotate(0, 0, 45);
+        }
+
+        if (Input.GetKeyUp("z"))
+        {
+            speed = 3.0f;
+            weapon.transform.Rotate(0, 0, -45);
+        }
+	}
+}
---
+++ Assets/Scripts/SimpleEnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyController : MonoBehaviour {
+    
+    public float speed = 1.5f;
+    //public Transform thisEnemy;
+    public int aggroDistance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var movespeed = speed * Time.deltaTime;
+        var player = GameObject.FindWithTag("Player");
+        var playerX = player.transform.position.x;
+        var playerY = player.transform.position.y;
+        var playerZ = player.transform.position.z;
+
+        float distanceBetweenPlayerAndEnemy = Vector3.Distance(player.transform.position, transform.position);
+
+        Debug.Log("Distance to Player:" + distanceBetweenPlayerAndEnemy);
+
+        if (distanceBetweenPlayerAndEnemy < aggroDistance)
+        {
+            Debug.Log("Start moving!");
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movespeed);
+            //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            //transform.Rotate(playerX, x, playerZ);
+            //transform.Translate(playerX, playerY, z);
+        }
+    }
+}
