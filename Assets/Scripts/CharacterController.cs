ce18e61799dbbaf4f05cd939366b3e4b3ee07df4
Vladimir <drumscum13@gmail.com>
2018-11-07 02:21:03 +0300

    Shooting
    Raycast shooting, response on ray colliding enemy

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
     GameObject gameObject;
     float speed = 3.0f;
+    bool shootingMode = false;
+    public int shootingRange = 10;
+
     // Use this for initialization
     void Start()
     {
@@ -15,20 +18,41 @@ public class CharacterController : MonoBehaviour {
 	void Update () {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        RaycastHit hit;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
         var weapon = GameObject.FindGameObjectWithTag("Weapon");
+        //Debug.Log(shootingMode);
 
+        //Shoot mode ready
         if (Input.GetKeyDown("z"))
         {
+            Debug.Log("Shooting mode");
+            shootingMode = true;
+            //Debug.Log(shootingMode);
             speed = 0f;
             weapon.transform.Rotate(0, 0, 45);
         }
 
+        //Shoot
+        if (Input.GetKey("z") && Input.GetKeyDown("c"))
+        {
+            //Debug.Log("POW!");
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 shootLine = forward * shootingRange;
+            if (Physics.Raycast(transform.position, forward, out hit))
+            {
+                Debug.Log("Object hit!");
+                print(hit.collider);
+            }
+            Debug.DrawRay(transform.position, forward * shootingRange, Color.red, 600);
+        }
+
         if (Input.GetKeyUp("z"))
         {
+            shootingMode = false;
             speed = 3.0f;
             weapon.transform.Rotate(0, 0, -45);
         }
--- Assets/Scripts/SimpleEnemyController.cs
+++ Assets/Scripts/SimpleEnemyController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SimpleEnemyController : MonoBehaviour {
     
     public float speed = 1.5f;
-    //public Transform thisEnemy;
     public int aggroDistance;
 
 	// Use this for initialization
@@ -15,26 +14,17 @@ public class SimpleEnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         var movespeed = speed * Time.deltaTime;
         var player = GameObject.FindWithTag("Player");
-        var playerX = player.transform.position.x;
-        var playerY = player.transform.position.y;
-        var playerZ = player.transform.position.z;
 
         float distanceBetweenPlayerAndEnemy = Vector3.Distance(player.transform.position, transform.position);
 
-        Debug.Log("Distance to Player:" + distanceBetweenPlayerAndEnemy);
+        //Debug.Log("Distance to Player:" + distanceBetweenPlayerAndEnemy);
 
         if (distanceBetweenPlayerAndEnemy < aggroDistance)
         {
-            Debug.Log("Start moving!");
+            //Debug.Log("Start moving!");
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movespeed);
-            //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-            //transform.Rotate(playerX, x, playerZ);
-            //transform.Translate(playerX, playerY, z);
         }
     }
 }
